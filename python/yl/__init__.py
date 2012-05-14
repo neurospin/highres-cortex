38ee1c5ce72f1ccfe779b917911335e8d01b0764
Yann Leprince <yann.leprince@cea.fr>
2012-05-14 08:44:34 +0000

    initialize Python module hierarchy
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perso/leprince/dev@48960 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/yl/__init__.py
@@ -0,0 +1,5 @@
+"""Yann Leprince's personal Python module in BioProj
+
+A collection of functions, classes, and various objects that I use in
+my Python scripts.  Please see README if you want to re-use.
+"""
