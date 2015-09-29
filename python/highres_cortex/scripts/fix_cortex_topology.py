179636b2a1b4d0940e1bc7da738e54702e511898
Yann Leprince <yann.leprince@telecom-paristech.org>
2015-09-29 18:55:26 +0200

    better error reporting for topology correction

--- python/highres_cortex/scripts/fix_cortex_topology.py
+++ python/highres_cortex/scripts/fix_cortex_topology.py
@@ -1,4 +1,39 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright Télécom ParisTech (2015).
+#
+# Contributor: Yann Leprince <yann.leprince@ylep.fr>.
+#
+# This file is part of highres-cortex, a collection of software designed
+# to process high-resolution magnetic resonance images of the cerebral
+# cortex.
+#
+# This software is governed by the CeCILL licence under French law and
+# abiding by the rules of distribution of free software. You can use,
+# modify and/or redistribute the software under the terms of the CeCILL
+# licence as circulated by CEA, CNRS and INRIA at the following URL:
+# <http://www.cecill.info/>.
+#
+# As a counterpart to the access to the source code and rights to copy,
+# modify and redistribute granted by the licence, users are provided only
+# with a limited warranty and the software's author, the holder of the
+# economic rights, and the successive licensors have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading, using, modifying and/or developing or reproducing the
+# software by the user in light of its specific status of scientific
+# software, that may mean that it is complicated to manipulate, and that
+# also therefore means that it is reserved for developers and experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and, more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL licence and that you accept its terms.
 
 import argparse
 import sys
@@ -12,8 +47,22 @@ def fix_cortex_topology_files(input_filename, output_filename,
                               filling_size, fclosing):
     """Call highres_cortex.cortex_topo.fix_cortex_topology on files."""
     input_volume = aims.read(input_filename)
-    output = highres_cortex.cortex_topo.fix_cortex_topology(
-        input_volume, filling_size, fclosing)
+
+    try:
+        output = highres_cortex.cortex_topo.fix_cortex_topology(
+            input_volume, filling_size, fclosing)
+    except OSError as exc:
+        print("error: the VipHomotopic command cannot be"
+              " found or executed ({0}). Please make sure that"
+              " Morphologist is properly installed and that the"
+              " command is in your PATH.".format(exc.strerror))
+        return 1
+    except subprocess.CalledProcessError as exc:
+        print("error: the VipHomotopic command returned an error code ({0})."
+              " Please inspect its output above for more information."
+              .format(exc.returncode))
+        return 1
+
     # BUG: aims.write offers no error checking, so the program will exit
     # successfully even if writing fails
     aims.write(output, output_filename)
