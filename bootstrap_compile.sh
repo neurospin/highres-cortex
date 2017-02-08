553f18ad50444de5ecc4685a828995760fec9eeb
Yann Leprince <y.leprince@fz-juelich.de>
2017-02-08 12:19:55 +0100

    Fix bootstrap_compile.sh: do not overwrite bv_maker.cfg
    Minor improvements:
    - pause for allowing customization of bv_maker.cfg (e.g. for passing cmake_options)
    - re-use existing brainvisa-cmake checkout

--- bootstrap_compile.sh
+++ bootstrap_compile.sh
@@ -1,15 +1,14 @@
 #! /bin/sh
 
-
 # Helper functions
 die() {
-    echo "Fatal error: $1" >&2
+    msg "Fatal error: $1"
     exit 1
 }
 
 msg() {
     tput smso  # bold
-    echo "$@"
+    echo "$@" >&2
     tput rmso  # bold off
 }
 
@@ -20,7 +19,7 @@ msg "This script will download and build highres-cortex and its dependencies."
 #####################################
 # Check if dependencies are installed
 #####################################
-if lsb_release -c | grep trusty 2>&1 >/dev/null; then
+if lsb_release -c 2>/dev/null | grep trusty >/dev/null; then
     # (Ubuntu 14.04 "trusty")
     # libqt4-sql-sqlite is not needed, but warnings show up when it is not present
     packages="subversion ca-certificates git cmake make gcc g++ gfortran pkg-config libblitz0-dev libsigc++-2.0-dev libxml2-dev libqt4-dev libboost-dev zlib1g-dev libtiff-dev libgsl0-dev python2.7-dev python-sip-dev python-numpy python-six libqt4-sql-sqlite"
@@ -51,7 +50,8 @@ fi
 # default value
 base_dir=$HOME/brainvisa
 
-printf 'Where do you want to bootstrap highres-cortex? [Default: %s] ' "$HOME/brainvisa"
+printf 'Where do you want to bootstrap highres-cortex? [Default: %s] ' \
+       "$HOME/brainvisa"
 read base_dir
 
 if [ -z "$base_dir" ]; then
@@ -61,31 +61,42 @@ fi
 mkdir -p -- "$base_dir" || die "cannot create directory '$base_dir'"
 cd -- "$base_dir" || die "cannot enter directory '$base_dir'"
 
-msg "Will now bootstrap highres-cortex under $base_dir"
+msg "Will now bootstrap highres-cortex under $PWD"
 
 
 ###########################
 # Bootstrap brainvisa-cmake
 ###########################
 
-brainvisa_cmake_svn=https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix
+if [ -x "$base_dir/brainvisa-cmake/bin/bv_maker" ]; then
+    bv_maker=$base_dir/brainvisa-cmake/bin/bv_maker
+    msg "
+An existing checkout of brainvisa-cmake was detected at
+$base_dir/brainvisa-cmake/bin/bv_maker
 
-svn export --non-interactive --username brainvisa --password Soma2009 "$brainvisa_cmake_svn" $base_dir/brainvisa-cmake || die "cannot download brainvisa-cmake"
+This script will carry on using it. If this causes problems, you can
+delete the $base_dir/brainvisa-cmake directory
+and run this script again to make a clean checkout."
+else
+    brainvisa_cmake_svn=https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/brainvisa-cmake/branches/bug_fix
 
-(
-    set -e
-    cd $base_dir/brainvisa-cmake
-    cmake -DCMAKE_INSTALL_PREFIX=. .
-    make install
-) || die "cannot bootstrap brainvisa-cmake"
+    svn export --non-interactive --username brainvisa --password Soma2009 "$brainvisa_cmake_svn" $base_dir/brainvisa-cmake || die "cannot download brainvisa-cmake"
+
+    (set -e
+        cd "$base_dir"/brainvisa-cmake
+        cmake -DCMAKE_INSTALL_PREFIX=. .
+        make install
+    ) || die "cannot bootstrap brainvisa-cmake"
+
+    bv_maker=$base_dir/brainvisa-cmake/bin/bv_maker
+fi
 
 
 ############################################################
 # Use brainvisa-cmake to download and compile highres-cortex
 ############################################################
 
-mkdir -p "$HOME/.brainvisa"
-cat <<EOF > "$HOME/.brainvisa/bv_maker.cfg"
+bv_maker_cfg_contents="\
 [ source $base_dir/source ]
   + brainvisa-cmake bug_fix
   + brainvisa-share bug_fix
@@ -103,32 +114,49 @@ cat <<EOF > "$HOME/.brainvisa/bv_maker.cfg"
   soma-io bug_fix $base_dir/source
   aims-free bug_fix $base_dir/source
   + $base_dir/source/highres-cortex
-EOF
+"
+
+mkdir -p "$HOME/.brainvisa"
+set -C  # do not overwrite existing configuration
+
+echo "$bv_maker_cfg_contents" > "$HOME/.brainvisa/bv_maker.cfg" 2>/dev/null
+if [ $? -ne 0 ] ; then
+    msg "\
+A bv_maker configuration already exists at $HOME/.brainvisa/bv_maker.cfg
+
+Make sure that it contains the following:
+
+$bv_maker_cfg_contents
+"
+fi
+
+msg "\
+Please adapt $HOME/.brainvisa/bv_maker.cfg now if needed, then
+press Enter to continue"
+read dummy
 
 
-BV_MAKER=$base_dir/brainvisa-cmake/bin/bv_maker
+msg "
+******************************************************
+* The next step (fetching sources of highres-cortex  *
+* dependencies) may ask you for identification. Use: *
+*    Username: brainvisa                             *
+*    Password: Soma2009                              *
+******************************************************
+"
 
-msg
-msg "******************************************************"
-msg "* The next step (fetching sources of highres-cortex  *"
-msg "* dependencies) may ask you for identification. Use: *"
-msg "*    Username: brainvisa                             *"
-msg "*    Password: Soma2009                              *"
-msg "******************************************************"
-msg
-
-"$BV_MAKER" --username brainvisa sources || die "fetching the sources failed"
-"$BV_MAKER" configure || die "configuring with CMake failed"
-"$BV_MAKER" build || die "build failed"
+"$bv_maker" --username brainvisa sources || die "fetching the sources failed"
+"$bv_maker" configure || die "configuring with CMake failed"
+"$bv_maker" build || die "build failed"
 
 # This is not needed anymore, use $base_dir/build/bin/bv_maker instead
 rm -rf "$base_dir"/brainvisa-cmake
 
-msg
-msg "You can now use highres-cortex."
-msg "Remember to set up the environment first with:"
-msg "  . $base_dir/build/bin/bv_env.sh $base_dir/build"
-msg
-msg "You can update highres-cortex by running:"
-msg "  $base_dir/build/bin/bv_maker"
-msg
+msg "
+You can now use highres-cortex.
+Remember to set up the environment first with:
+  . $base_dir/build/bin/bv_env.sh $base_dir/build
+
+You can update highres-cortex by running:
+  $base_dir/build/bin/bv_maker
+"
