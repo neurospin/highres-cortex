fc2cf99c2f93a18810c066d99cef4b2dc34ee6b5
Yann Leprince <y.leprince@fz-juelich.de>
2017-08-08 16:09:19 +0200

    Option to add noise to synthetic data
    Additionally, the generated data was restricted to spheres (ellipsoids were unused)

--- python/highres_cortex/test/synthetic_data.py
+++ python/highres_cortex/test/synthetic_data.py
@@ -51,26 +51,25 @@ def _convert_to_float_triple(param):
     return tuple(float(item) for item in param)
 
 def make_cortex_sphere_classif(inner_radius, outer_radius,
-                               voxel_size, margin=None):
-    inner_radius = _convert_to_float_triple(inner_radius)
-    outer_radius = _convert_to_float_triple(outer_radius)
+                               voxel_size, margin=None,
+                               noise=None, sigma=None):
     voxel_size = _convert_to_float_triple(voxel_size)
     if margin is None:
         margin = max(voxel_size)
     margin = _convert_to_float_triple(margin)
 
-    for i in range(3):
-        assert outer_radius[i] > inner_radius[i] > 0
+    assert outer_radius > inner_radius > 0
 
-    size = [int(math.ceil(2 * (ax_radius + ax_margin) / ax_voxel_size))
-            for ax_radius, ax_margin, ax_voxel_size
-            in zip(outer_radius, margin, voxel_size)]
+    size = [int(math.ceil(2 * (outer_radius + ax_margin) / ax_voxel_size))
+            for ax_margin, ax_voxel_size
+            in zip(margin, voxel_size)]
 
     classif_aimsdata = aims.AimsData(size[0], size[1], size[2], dtype="S16")
     classif_aimsdata.setSizeXYZT(*voxel_size)
     classif_volume = classif_aimsdata.volume()
     emplace_cortex_sphere_classif(classif_volume,
-                                  inner_radius, outer_radius, margin)
+                                  inner_radius, outer_radius,
+                                  margin=margin, noise=noise, sigma=sigma)
     return classif_volume
 
 def make_centred_coord_grids(classif_volume):
@@ -87,27 +86,35 @@ def make_centred_coord_grids(classif_volume):
 
 
 def emplace_cortex_sphere_classif(classif_volume,
-                                  inner_radius, outer_radius, margin):
-    inner_radius = _convert_to_float_triple(inner_radius)
-    outer_radius = _convert_to_float_triple(outer_radius)
+                                  inner_radius, outer_radius,
+                                  margin, noise, sigma):
     margin = _convert_to_float_triple(margin)
 
     size = (classif_volume.getSizeX(),
             classif_volume.getSizeY(),
             classif_volume.getSizeZ())
     voxel_size = classif_volume.header()["voxel_size"][:3]
+    assert outer_radius > inner_radius > 0
     for i in range(3):
-        assert outer_radius[i] > inner_radius[i] > 0
-        assert size[i] * voxel_size[i] >= 2 * outer_radius[i]
+        assert size[i] * voxel_size[i] >= 2 * outer_radius
 
     grids = make_centred_coord_grids(classif_volume)
 
     np_classif = numpy.asarray(classif_volume)
     classif_volume.fill(0)
-    np_classif[sum(grid ** 2 / radius ** 2
-                   for radius, grid in zip(outer_radius, grids)) < 1] = 100
-    np_classif[sum(grid ** 2 / radius ** 2
-                   for radius, grid in zip(inner_radius, grids)) < 1] = 200
+
+    distance_to_centre = sum(grid ** 2 for grid in grids)
+    if noise:
+        distance_to_centre += make_noise_array(noise, sigma, size)
+    np_classif[distance_to_centre < outer_radius ** 2] = 100
+    np_classif[distance_to_centre < inner_radius ** 2] = 200
+
+
+def make_noise_array(noise, sigma, size):
+    import scipy.ndimage.filters
+    arr = noise * numpy.random.randn(*size)
+    scipy.ndimage.filters.gaussian_filter(arr, sigma, output=arr)
+    return arr
 
 def _make_similar_volume(data_array, ref):
     volume = aims.Volume(data_array)
@@ -115,7 +122,7 @@ def _make_similar_volume(data_array, ref):
     return volume
 
 def make_sphere_and_reference_result(inner_radius, outer_radius, voxel_size,
-                                     margin=None):
+                                     margin=None, noise=None, sigma=None):
     inner_radius = float(inner_radius)
     outer_radius = float(outer_radius)
     assert outer_radius > inner_radius > 0
@@ -124,7 +131,8 @@ def make_sphere_and_reference_result(inner_radius, outer_radius, voxel_size,
     thickness = outer_radius - inner_radius
 
     classif_volume = make_cortex_sphere_classif(inner_radius, outer_radius,
-                                                voxel_size, margin=margin)
+                                                voxel_size, margin=margin,
+                                                noise=noise, sigma=sigma)
     grids = make_centred_coord_grids(classif_volume)
     distance_to_centre = numpy.sqrt(sum(grid ** 2 for grid in grids))
 
@@ -156,7 +164,8 @@ def make_sphere_and_reference_result(inner_radius, outer_radius, voxel_size,
             _make_similar_volume(equivolumic_metric, ref=classif_volume))
 
 def write_sphere_and_reference_result(inner_radius, outer_radius, voxel_size,
-                                      dir=".", margin=None):
+                                      dir=".", margin=None,
+                                      noise=None, sigma=None):
     if not os.path.isdir(dir):
         os.makedirs(dir)
     inner_radius = float(inner_radius)
@@ -171,7 +180,8 @@ def write_sphere_and_reference_result(inner_radius, outer_radius, voxel_size,
      curvature,
      equivolumic_metric) = (
          make_sphere_and_reference_result(
-            inner_radius, outer_radius, voxel_size, margin=margin))
+             inner_radius, outer_radius, voxel_size, margin=margin,
+             noise=noise, sigma=sigma))
 
     np_thickness = (outer_radius - inner_radius) * (
         numpy.ones_like(distance_to_white))
@@ -206,9 +216,13 @@ if __name__ == "__main__":
     parser.add_argument("thickness", type=float)
     parser.add_argument("voxel_size", type=float)
     parser.add_argument("--margin", type=float, default=None)
+    parser.add_argument("--noise", type=float, default=None)
+    parser.add_argument("--sigma", type=float, default=None)
     args = parser.parse_args()
     write_sphere_and_reference_result(
         args.inner_radius,
         args.inner_radius + args.thickness,
         args.voxel_size,
-        margin=args.margin)
+        margin=args.margin,
+        noise=args.noise,
+        sigma=args.sigma)
